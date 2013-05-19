f2073261b56e8cd63b4a63b8d0719388c43139be
peepo3663 <peepo157@gmail.com>
2013-05-20 01:47:33 +0700

    test link can create

--- test/unit/linklike/LinkTests.groovy
+++ test/unit/linklike/LinkTests.groovy
@@ -11,7 +11,12 @@ import org.junit.*
 @TestFor(Link)
 class LinkTests {
 
-    void testSomething() {
-       fail "Implement me"
-    }
+
+	void testLink() {
+		def testLink = new Link(title: '1234', url: '5678');
+
+		assert testLink.title == '1234'
+		assert testLink.url == '5678'
+		assert testLink.like == 0
+	}
 }
